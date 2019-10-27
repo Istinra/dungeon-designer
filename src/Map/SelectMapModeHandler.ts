0ede579b75afbe6376d814fecff33e5b1ca4b84f
Istinra <forsaken.panda@gmail.com>
2019-10-27 12:58:29 +1100

    Select working for rooms

--- src/Map/SelectMapModeHandler.ts
+++ src/Map/SelectMapModeHandler.ts
@@ -1,21 +1,58 @@
 import {MapModeHandler} from "./MapModeHandler";
-import {MapState, Point, SelectedState} from "../state";
+import {MapState, ObjectType, Point, SelectedState} from "../state";
+import {GRID_IN_PX} from "./DungonMapConstants";
 
 export class SelectMapModeHandler implements MapModeHandler {
+
+    private mousePoint: Point = {x: 0, y: 0};
 
     constructor(private onSelection: (selected: SelectedState) => void) {
     }
 
+    onMouseMove(state: MapState, point: Point): void {
+        this.mousePoint = {x: point.x / GRID_IN_PX, y: point.y / GRID_IN_PX};
+    }
 
     draw(state: MapState, ctx: CanvasRenderingContext2D): void {
     }
 
     onMapClicked(state: MapState): void {
-        //TODO Intersect test
+        for (let i = 0; i < state.rooms.length; i++) {
+            const room = state.rooms[i];
+            if (this.testRoom(room)) {
+                this.onSelection({type: ObjectType.ROOM, index: i});
+                return;
+            }
+        }
+        this.onSelection({type: ObjectType.MAP, index: 0});
     }
 
-    onMouseMove(state: MapState, point: Point): void {
+    private testRoom(room): boolean {
+        let count = 0;
+        for (let i = 0; i < room.points.length - 1; i++) {
+            if (this.intersectsLine(room.points[i], room.points[i + 1])) {
+                count++;
+            }
+        }
+        if (this.intersectsLine(room.points[room.points.length - 1], room.points[0])) {
+            count++;
+        }
+        return count % 2 === 1;
     }
 
+    private intersectsLine(from, to) {
+        if (from.y <= to.y) {
+            if (this.mousePoint.y <= from.y || this.mousePoint.y > to.y ||
+                (this.mousePoint.x >= from.x && this.mousePoint.x >= to.x)) {
+                return false;
+            } else if (this.mousePoint.x < from.x && this.mousePoint.x < to.x) {
+                return true;
+            } else {
+                return (this.mousePoint.y - from.y) / (this.mousePoint.x - from.x) > (to.y - from.y) / (to.x - from.x);
+            }
+        } else {
+            return this.intersectsLine(to, from);
+        }
+    }
 
 }
